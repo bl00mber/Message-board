c8c141a2cec258a696d6fe3e5d38d0a2d9bcfff0
bl00mber <bloomber111@gmail.com>
2016-09-14 00:46:26 +0500

    Tab fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,19 @@
 var	my_news	=	[
-		{
-				author:	'Yves G',
-				text:	"I didn't try the food. I wasn't there to eat, but to relax.",
-        bigText: 'Just passed the Consumer Review Fairness Act to prevent companies from using gag orders to stop customers from writing honest reviews online.'
-
-		},
-		{
-				author:	'Jonathan Haynes',
-				text:	'I worked at Lorem Ipsum full-time (More than a year)',
-        bigText: "On Friday the company buys lunches for everyone. But they've also decreased how much they will cover over time. It went from $20, down to $15 before tip then down to $15 with tip."
-		},
-		{
-				author:	'Steve Campion',
-				text:	'62% of job seekers feel better about companies that respond to reviews',
-        bigText: 'Astonishing result from blind resume reviews by tech companies. Should this become a best practice in hiring?'
-		}
+  {
+    author:	'Yves G',
+    text:	"I didn't try the food. I wasn't there to eat, but to relax.",
+    bigText: 'Just passed the Consumer Review Fairness Act to prevent companies from using gag orders to stop customers from writing honest reviews online.'
+  },
+  {
+    author:	'Jonathan Haynes',
+    text:	'I worked at Lorem Ipsum full-time (More than a year)',
+    bigText: "On Friday the company buys lunches for everyone. But they've also decreased how much they will cover over time. It went from $20, down to $15 before tip then down to $15 with tip."
+  },
+  {
+    author:	'Steve Campion',
+    text:	'62% of job seekers feel better about companies that respond to reviews',
+    bigText: 'Astonishing result from blind resume reviews by tech companies. Should this become a best practice in hiring?'
+  }
 ];
 
 var Article = React.createClass({
@@ -26,32 +25,32 @@ var Article = React.createClass({
     })
   },
 
-	getInitialState: function() {
-		return {
-			hidden: true
-		}
-	},
+  getInitialState: function() {
+    return {
+      hidden: true
+    }
+  },
 
-	readMoreClick: function(e) {
-		e.preventDefault();
-		this.setState({hidden: false});
-	},
+  readMoreClick: function(e) {
+    e.preventDefault();
+    this.setState({hidden: false});
+  },
 
   render: function() {
     var author = this.props.data.author,
         text = this.props.data.text,
         bigText = this.props.data.bigText,
-				hidden = this.state.hidden;
+        hidden = this.state.hidden;
 
     return (
       <div className="article">
         <p className="news__author">{author}:</p>
         <p className="news__text">{text}</p>
         <a href="#"
-					onClick={this.readMoreClick}
-					className={'news__readmore' + (hidden ? '' : ' hidden')}>
-					More..
-				</a>
+          onClick={this.readMoreClick}
+          className={'news__readmore' + (hidden ? '' : ' hidden')}>
+          More..
+        </a>
         <p className={'news__big-text' + (hidden ? ' hidden' : '')}>{bigText}</p>
       </div>
     )
@@ -59,9 +58,9 @@ var Article = React.createClass({
 });
 
 var News = React.createClass({
-	propTypes: {
-		data: React.PropTypes.array.isRequired
-	},
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
 
   render: function() {
     var data = this.props.data,
@@ -70,9 +69,9 @@ var News = React.createClass({
     if (data.length > 0) {
       newsTemplate = data.map(function(item, index) {
         return (
-  				<div key={index}>
-  						<Article data={item}/>
-  				</div>
+          <div key={index}>
+              <Article data={item}/>
+          </div>
         )
       })
     } else {
@@ -97,41 +96,41 @@ var News = React.createClass({
 
 var TopForm = React.createClass({
 
-	onBtnClickHandler: function() {
-		console.log(ReactDOM.findDOMNode(this.refs.testInput).value);
-		console.dir(this);
-	},
+  onBtnClickHandler: function() {
+    console.log(ReactDOM.findDOMNode(this.refs.testInput).value);
+    console.dir(this);
+  },
 
-	render: function() {
-		return (
-			<div>
-				<input className='test-input'
-				 placeholder='Enter a value'
-				 defaultValue=''
-				 ref='testInput'/>
-				<input className='test-button'
-				 type='button'
-				 value='send'
-				 onClick={this.onBtnClickHandler}/>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div>
+        <input className='test-input'
+         placeholder='Enter a value'
+         defaultValue=''
+         ref='testInput'/>
+        <input className='test-button'
+         type='button'
+         value='send'
+         onClick={this.onBtnClickHandler}/>
+      </div>
+    );
+  }
 });
 
 var	App	=	React.createClass({
-	render:	function() {
-		return (
-			<div className="app">
-					<h2>News</h2>
-					<TopForm/>
+  render:	function() {
+    return (
+      <div className="app">
+          <h2>News</h2>
+          <TopForm/>
           <br/><br/>
           <News data={my_news}/>
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
-		<App/>,
-		document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
